2d5310fe013d7dc4b91be2ede0141a310f70adc6
Vincenzo <vincenzo.cavataio1@gmail.com>
2021-04-15 00:29:35 +0200

    spaces removed

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
-var path = require('path')
-;
+var path = require('path');
+
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-
 
 app.listen(process.env.PORT || 5000);
